52f9fb8b578673f69321112f0ff44f0a51f0167c
Anthony Giuliano <agiulian@nd.edu>
2019-03-19 14:46:13 -0400

    updated login

--- JS/login.js
+++ JS/login.js
@@ -1,33 +1,28 @@
 firebase.auth().onAuthStateChanged(function(user){
   if(user){
     // User is signed in
-    // window.location.href = "../table.html";
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
+    window.location.href = "../table.html";
+    console.log('success');
   }
   else{
     // No user is signed in
-    document.getElementById("login_div").style.display = "block";
-    document.getElementById("user_div").style.display = "none";
+    // sit and wait for user to sign in
   }
 });
 
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-function login(){
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error){
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
     // Handle errors here
     var errorCode = error.code;
     var errorMessage = error.message;
     
     window.alert("Error : " + errorMessage);
   });
-  
-  console.log(userEmail + " " + userPass);
-}
-
-function logout(){
-  firebase.auth().signOut();
-}
+});
